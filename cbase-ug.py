61b8a34a1beb0f09121b2c0ea8f8012515438a14
normann <s.kotelnikov@rts.ru>
2026-04-03 16:31:44 +0300

    Changes to be committed: 	modified:   cbase-ug.py

--- cbase-ug.py
+++ cbase-ug.py
@@ -208,14 +208,6 @@ def main():
                     if create_items == True:
                         AddressList.addr_list_add_items(rule_item, addr_list_type, all_addr_lists_name, addr_list_manager)
                         logging.info(f"{vpnlogin} new items created")
-                        # for ip_list in rule_item[addr_list_type]:
-                        #     ip_list_name = ip_list.get('name')
-                        #     ip_list_id = all_addr_lists_name.get(ip_list_name).get('id')
-                        #     new_values = ip_list.get('items')
-                        #     for value in new_values:
-                        #         new_list_value = {'value': value}
-                                # logging.info(f"{vpnlogin} new_list_value {new_list_value}")
-                                # addr_list_manager.create_list_item(ip_list_id, new_list_value)
                 
                 logging.info(f"{vpnlogin} addr_list_add done")
                 
@@ -263,15 +255,6 @@ def main():
                     addr_list_manager.delete_list_items(current_addr_list_id, item)
                 AddressList.addr_list_add_items(rule_item, addr_list_type, all_addr_lists_name, addr_list_manager)
                 logging.info(f"{vpnlogin} new items created")
-                # for ip_list in rule_item[addr_list_type]:
-                #     ip_list_name = ip_list.get('name')
-                #     ip_list_id = all_addr_lists_name.get(ip_list_name).get('id')
-                #     new_values = ip_list.get('items')
-                #     new_values = ip_list.get('items')
-                #     for value in new_values:
-                #         new_list_value = {'value': value}
-                #         logging.info(f"{vpnlogin} new_list_value {new_list_value}")
-                #         addr_list_manager.create_list_item(ip_list_id, new_list_value)
         
         elif args.delete:
             rule_manager.delete_rule(current_rule_id)
