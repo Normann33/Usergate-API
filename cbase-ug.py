89d4af08e066bae6830a27487f8a36f419f316da
normann <s.kotelnikov@rts.ru>
2026-04-13 17:12:13 +0300

    Changes to be committed: 	modified:   cbase-ug.py

--- cbase-ug.py
+++ cbase-ug.py
@@ -266,8 +266,8 @@ def main():
             if db_iplist_name != current_addr_list_name:
                 addr_list_to_update = {'type': 'network', 'name': db_iplist_name}
                 print(addr_list_manager.update_list(current_addr_list_id, addr_list_to_update))
-                print(addr_list_manager.get_addr_list_items(current_addr_list_id))
                 for item in addr_list_manager.get_addr_list_items(current_addr_list_id):
+                    print(addr_list_manager.get_addr_list_items(current_addr_list_id))
                     print(f'Item {item} in address list {current_addr_list_id}')
                     addr_list_manager.delete_list_items(current_addr_list_id, item)
                 AddressList.addr_list_add_items(rule_item, addr_list_type, all_addr_lists_name, addr_list_manager)
